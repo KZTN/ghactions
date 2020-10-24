03bcf63635f11c71054422a7288d7f35df3266fa
Kaio César <kaio.araujo@mobiliza.com.br>
2020-10-24 12:55:05 -0300

    ✅ Test: actions
    ook

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,24 @@ import routes from "./routes";
 import "./bootstrap";
 
 class App {
+
   public app = express.application;
+
   public constructor() {
     this.app = express();
     this.middlewares();
     this.routes();
   }
+  
   private middlewares(): void {
     this.app.use(cors());
     this.app.use(morgan("dev"));
   }
+
   private routes(): void {
     this.app.use(routes);
   }
+
 }
+
 export default new App().app;
