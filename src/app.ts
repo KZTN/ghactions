7575e9704f094a2128712f9b1da4cc024109df27
Kaio César <kaio.araujo@mobiliza.com.br>
2020-11-08 11:56:57 -0300

    test: danger

--- src/app.ts
+++ src/app.ts
@@ -6,18 +6,22 @@ import "./bootstrap";
 
 class App {
   public app = express.application;
+
   public constructor() {
     this.app = express();
     this.middlewares();
     this.routes();
   }
+
   private middlewares(): void {
     this.app.use(cors());
     this.app.use(morgan("dev"));
   }
+
   private routes(): void {
     this.app.use(routes);
   }
+  
 }
 
 export default new App().app;
